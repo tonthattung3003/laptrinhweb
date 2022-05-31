4db9e18db34852d6257e9b4e27e5ce6e68958c42
tonthattung3003 <106566355+tonthattung3003@users.noreply.github.com>
2022-05-31 23:05:37 +0700

    first commit

---
+++ PizzaPlace/Client/Pages/CustomerEntry.razor.cs
@@ -0,0 +1,27 @@
+﻿using PizzaPlace.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace PizzaPlace.Client.Pages
+{
+    partial class CustomerEntry
+    {
+        [Parameter]
+        public string Title { get; set; } = default!;
+        [Parameter]
+        public string ButtonTitle { get; set; } = default!;
+        [Parameter]
+        public string ButtonClass { get; set; } = default!;
+        [Parameter]
+        public Customer Customer { get; set; } = default!;
+        [Parameter]
+        public EventCallback ValidSubmit { get; set; } = default!;
+
+        private void FieldChanged(string fieldName)
+        {
+            CustomerChanged.InvokeAsync(Customer);
+            isInvalid = !inputWatcher.Validate();
+        }
+        private InputWatcher inputWatcher = default!;
+        bool isInvalid = true;
+    }
+}
